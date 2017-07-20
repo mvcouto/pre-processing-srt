6a5679f02177f5492bbc78725a58ebd584d48e4c
Tiago Zamperini <tiagozamperini@live.com>
2017-07-20 01:15:02 -0300

    melhorando conexão com o banco

--- app.js
+++ app.js
@@ -36,69 +36,69 @@ var router = express.Router();
 var spawn = require('child_process').spawn;
 spawn('node', ['pre-processing-srt.js', 'inglourious-basterds-english']);
 
-var connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL || {
+var pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL || {
     host: 'localhost',
     user: 'root',
     database: 'crowdsub'
 });
 
-connection.connect(function (err) {
-    if (!err) {
+pool.getConnection(function(err, connection) {
+	if (!err) {
         console.log("Database is connected ... nn");
     } else {
         console.log("Error connecting database ... nn");
     }
+
+
+	global.fifoTarefaEnviada = new Array();
+	global.fifoTarefaRecebida = new Array();
+
+	// Task 1 - Extrair legendas do vídeo
+	var creatorTask1 = require("./services/CreatorTask1");
+	router.get('/task1', function (req, res) {
+		console.log("GET /task1");
+		console.log('Body: ' + JSON.stringify(req.body));
+
+		creatorTask1.getItemId(connection, res);
+	});
+
+	var submissionTask1Service = require("./services/SubmissionTask1");
+	router.post('/task1', function (req, res) {
+		console.log("POST /task1");
+		console.log('Body: ' + JSON.stringify(req.body));
+
+		submissionTask1Service.insertSubmission(res,
+			connection,
+			req.body.id_video,
+			req.body.legenda,
+			req.body.tinicial,
+			req.body.tfinal,
+			req.body.fingerprint);
+	});
+
+
+	// Task 2 - Extrair melhor legendas, dentre as legendas sugeridas pelos usuários
+	router.get('/task2', function (req, res) {
+		console.log("GET /task2");
+		console.log('Body: ' + JSON.stringify(req.body));
+
+		res.status(200).send("");
+	});
+
+	var submissionTask2Service = require("./services/SubmissionTask2");
+	router.post('/task2', function (req, res) {
+		console.log("POST /task2");
+		console.log('Body: ' + JSON.stringify(req.body));
+
+		submissionTask2Service.insertSubmission(res,
+			connection,
+			req.body.id_legenda_escolhida,
+			req.body.fingerprint);
+	});
+
+
+	app.use('/api', router);
+
+	app.listen(port);
+	console.log('Servidor rodando na porta: ' + port);
 });
-
-
-global.fifoTarefaEnviada = new Array();
-global.fifoTarefaRecebida = new Array();
-
-// Task 1 - Extrair legendas do vídeo
-var creatorTask1 = require("./services/CreatorTask1");
-router.get('/task1', function (req, res) {
-    console.log("GET /task1");
-    console.log('Body: ' + JSON.stringify(req.body));
-
-    creatorTask1.getItemId(connection, res);
-});
-
-var submissionTask1Service = require("./services/SubmissionTask1");
-router.post('/task1', function (req, res) {
-    console.log("POST /task1");
-    console.log('Body: ' + JSON.stringify(req.body));
-
-    submissionTask1Service.insertSubmission(res,
-        connection,
-        req.body.id_video,
-        req.body.legenda,
-        req.body.tinicial,
-        req.body.tfinal,
-        req.body.fingerprint);
-});
-
-
-// Task 2 - Extrair melhor legendas, dentre as legendas sugeridas pelos usuários
-router.get('/task2', function (req, res) {
-    console.log("GET /task2");
-    console.log('Body: ' + JSON.stringify(req.body));
-
-    res.status(200).send("");
-});
-
-var submissionTask2Service = require("./services/SubmissionTask2");
-router.post('/task2', function (req, res) {
-    console.log("POST /task2");
-    console.log('Body: ' + JSON.stringify(req.body));
-
-    submissionTask2Service.insertSubmission(res,
-        connection,
-        req.body.id_legenda_escolhida,
-        req.body.fingerprint);
-});
-
-
-app.use('/api', router);
-
-app.listen(port);
-console.log('Servidor rodando na porta: ' + port);
