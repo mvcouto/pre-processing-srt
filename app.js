2e921bc760afb95d39169916f41d5d516c044735
Tiago Zamperini <tiagozamperini@live.com>
2017-07-20 00:41:33 -0300

    pemitir origem das requisiçoes

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({extended: true}));
